337d3584924fe1b506cd13e71c6bf6920c527904
Darlan Machado <dr.bsi@hotmail.com>
2022-05-30 11:49:53 -0300

    Implementação de find all

--- src/main/java/br/com/axe/api/services/UserService.java
+++ src/main/java/br/com/axe/api/services/UserService.java
@@ -2,6 +2,10 @@ package br.com.axe.api.services;
 
 import br.com.axe.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+
+    List<User> findAll();
 }
--- src/main/java/br/com/axe/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/axe/api/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import br.com.axe.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,10 @@ public class UserServiceImpl implements UserService {
     public User findById(Integer id) {
         Optional<User> oUser = repo.findById(id);
         return oUser.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repo.findAll();
     }
 }
