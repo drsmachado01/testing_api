0d2efa8b5d3c5517268d3d3a6f1704f1b2389a3b
Darlan Machado <dr.bsi@hotmail.com>
2022-06-01 00:35:08 -0300

    Implementação de testes unitários para a classe UserResource

---
+++ src/test/java/br/com/axe/api/resources/UserResourceTest.java
@@ -0,0 +1,157 @@
+package br.com.axe.api.resources;
+
+import br.com.axe.api.domain.User;
+import br.com.axe.api.domain.dto.UserDTO;
+import br.com.axe.api.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserResourceTest {
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+
+    private UserDTO userDTO;
+
+    private static final Integer ID = 1;
+    private static final String NAME = "Astolpho Pamphilo";
+    private static final String EMAIL = "as.tolpho@email.com";
+    private static final String PWD = "123@qazWSX";
+    private static final String NEW_EMAIL = "astrogild@email.com";
+
+    private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado!";
+
+    private static final String EMAIL_JA_CADASTRADO_NO_SISTEMA = "Email já cadastrado no sistema!";
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        init();
+    }
+
+    @Test
+    void findById() {
+        when(userService.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        UserDTO respUser = response.getBody();
+        assertEquals(ID, respUser.getId());
+        assertEquals(NAME, respUser.getName());
+        assertEquals(EMAIL, respUser.getEmail());
+    }
+
+    @Test
+    void findByIdWhenDataIntegrityVuolationExceptionIsThrown() {
+        when(userService.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        UserDTO respUser = response.getBody();
+        assertEquals(ID, respUser.getId());
+        assertEquals(NAME, respUser.getName());
+        assertEquals(EMAIL, respUser.getEmail());
+    }
+
+    @Test
+    void findAll() {
+        when(userService.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        List<UserDTO> users = response.getBody();
+        assertEquals(1, users.size());
+        UserDTO respUser = users.get(0);
+        assertEquals(1, respUser.getId());
+        assertEquals(ID, respUser.getId());
+        assertEquals(NAME, respUser.getName());
+        assertEquals(EMAIL, respUser.getEmail());
+
+    }
+
+    @Test
+    void create() {
+        when(userService.create(any())).thenReturn(user);
+        when(mapper.map(userDTO, User.class)).thenReturn(user);
+        when(mapper.map(user, UserDTO.class)).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.create(userDTO);
+
+        assertNotNull(response);
+        assertNull(response.getBody());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test
+    void createFailedOnDataIntegrityViolationExceptionIsThrown() {
+        when(userService.create(any())).thenReturn(user);
+        when(mapper.map(userDTO, User.class)).thenReturn(user);
+        when(mapper.map(user, UserDTO.class)).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.create(userDTO);
+
+        assertNotNull(response);
+        assertNull(response.getBody());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test
+    void update() {
+        when(userService.update(anyInt(), any())).thenReturn(user);
+        when(mapper.map(userDTO, User.class)).thenReturn(user);
+        when(mapper.map(user, UserDTO.class)).thenReturn(userDTO);
+        ResponseEntity<UserDTO> response = resource.update(ID, userDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        UserDTO respUser = response.getBody();
+        assertEquals(ID, respUser.getId());
+        assertEquals(NAME, respUser.getName());
+        assertEquals(EMAIL, respUser.getEmail());
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void init() {
+        user = User.builder().id(ID).name(NAME).email(EMAIL).password(PWD).build();
+        userDTO = UserDTO.builder().id(ID).name(NAME).email(EMAIL).password(PWD).build();
+    }
+}
--- src/test/java/br/com/axe/api/services/UserServiceTest.java
+++ src/test/java/br/com/axe/api/services/UserServiceTest.java
@@ -1,8 +1,9 @@
 package br.com.axe.api.services;
 
 import br.com.axe.api.domain.User;
-import br.com.axe.api.domain.dto.UserDTO;
 import br.com.axe.api.repositories.UserRepository;
+import br.com.axe.api.services.exceptions.DataIntegrityViolationException;
+import br.com.axe.api.services.exceptions.ObjectNotFoundException;
 import br.com.axe.api.services.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,11 +19,12 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceTest {
-
     @InjectMocks
     private UserServiceImpl userService;
 
@@ -34,14 +36,17 @@ class UserServiceTest {
 
     private User user;
 
-    private UserDTO userDTO;
-
     private Optional<User> optUser;
 
     private static final Integer ID = 1;
     private static final String NAME = "Astolpho Pamphilo";
     private static final String EMAIL = "as.tolpho@email.com";
     private static final String PWD = "123@qazWSX";
+    private static final String NEW_EMAIL = "astrogild@email.com";
+
+    private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado!";
+
+    private static final String EMAIL_JA_CADASTRADO_NO_SISTEMA = "Email já cadastrado no sistema!";
 
     @BeforeEach
     void setUp() {
@@ -61,17 +66,31 @@ class UserServiceTest {
     }
 
     @Test
+    void findByIdObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+        try {
+            userService.findById(1);
+        } catch (Exception ex) {
+            assertObjectNotFoundException(ex);
+        }
+    }
+
+    @Test
     void findAll() {
         when(repository.findAll()).thenReturn(List.of(user));
         List<User> users = userService.findAll();
         assertNotNull(users);
         assertEquals(1, users.size());
-        assertEquals(ID, users.get(0).getId());
+        User user0 = users.get(0);
+        assertEquals(User.class, user0.getClass());
+        assertEquals(NAME, user0.getName());
+        assertEquals(EMAIL, user0.getEmail());
+        assertEquals(PWD, user0.getPassword());
     }
 
     @Test
     void create() {
-        when(repository.save(user)).thenReturn(user);
+        when(repository.save(any())).thenReturn(user);
         User newUser = userService.create(user);
         assertNotNull(newUser);
         assertEquals(ID, newUser.getId());
@@ -80,16 +99,90 @@ class UserServiceTest {
     }
 
     @Test
+    void testCreateWithAnExistingEmail() {
+        when(repository.findByEmail(anyString())).thenReturn(optUser);
+        try {
+            user.setId(null);
+            userService.create(user);
+        } catch (Exception ex) {
+            assertDataIntegrityViolationException(ex);
+        }
+    }
+
+    @Test
     void update() {
+        when(repository.findById(anyInt())).thenReturn(optUser);
+        when(repository.findByEmail(anyString())).thenReturn(optUser);
+        when(repository.save(any())).thenReturn(user);
+        User updUser = user;
+        String newPwd = "123@QAZwsx";
+        updUser.setPassword(newPwd);
+        updUser = userService.update(ID, updUser);
+        assertNotNull(updUser);
+        assertEquals(ID, updUser.getId());
+        assertEquals(NAME, updUser.getName());
+        assertEquals(EMAIL, updUser.getEmail());
+        assertEquals(newPwd, updUser.getPassword());
+    }
+
+    @Test
+    void updateWhenObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+        try {
+            userService.update(ID, user);
+        } catch (Exception ex) {
+            assertObjectNotFoundException(ex);
+        }
+    }
+
+    @Test
+    void updateWhenEmailAlreadyExists() {
+        when(repository.findById(anyInt())).thenReturn(optUser);
+    when(repository.findByEmail(anyString())).thenReturn(
+            Optional.ofNullable(User.builder()
+                    .id(2).name("Astrogildo Genova").email("astrogildo@email.com").password("123@WSXqaz")
+                    .build()));
+        try {
+            User updUser = user;
+            String newPwd = "123@QAZwsx";
+            updUser.setPassword(newPwd);
+            updUser.setEmail(NEW_EMAIL);
+            userService.update(ID, updUser);
+        } catch (Exception ex) {
+            assertDataIntegrityViolationException(ex);
+        }
     }
 
     @Test
     void delete() {
+        when(repository.findById(anyInt())).thenReturn(optUser);
+        doNothing().when(repository).delete(any());
+        userService.delete(ID);
+        Mockito.verify(repository, times(1)).delete(any());
+    }
+
+    @Test
+    void deleteWhenObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+        try {
+            userService.delete(ID);
+        } catch (Exception ex) {
+            assertObjectNotFoundException(ex);
+        }
+    }
+
+    private void assertObjectNotFoundException(Exception ex) {
+        assertEquals(ObjectNotFoundException.class, ex.getClass());
+        assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
+    }
+
+    private void assertDataIntegrityViolationException(Exception ex) {
+        assertEquals(DataIntegrityViolationException.class, ex.getClass());
+        assertEquals(EMAIL_JA_CADASTRADO_NO_SISTEMA, ex.getMessage());
     }
 
     private void init() {
         user = User.builder().id(ID).name(NAME).email(EMAIL).password(PWD).build();
-        userDTO = UserDTO.builder().id(ID).name(NAME).email(EMAIL).password(PWD).build();
         optUser = Optional.of(User.builder().id(ID).name(NAME).email(EMAIL).password(PWD).build());
     }
 }
